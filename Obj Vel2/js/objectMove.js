f6555bcf909e11dc45004ac7655e23af3d8c4315
pedroosoares <soarespedro.231@gmail.com>
2018-09-28 00:23:33 +0100

    mov3

--- Obj Vel2/js/objectMove.js
+++ Obj Vel2/js/objectMove.js
@@ -3,64 +3,8 @@
 
 var camera, scene, renderer;
 var material;
-var ball;
+var chair;
 
-function createBall(x,y,z) {
-    'use strict';
-
-    ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0};
-
-    var material = new THREE.MeshBasicMaterial({color:0xff0000, wireframe: true});
-    var geometry = new THREE.SphereGeometry(4,10,10);
-    var mesh = new THREE.Mesh(geometry,material);
-
-    ball.add(mesh);
-    ball.position.set(x,y,z);
-
-    scene.add(ball);
-}
-
-function addTableTop(obj, x,y,z) {
-    'use strict';
-    //var geometry = new THREE.CubeGeometry(70,3,40);
-    var geometry = new THREE.CubeGeometry(70, 4, 40);
-    
-    var mesh = new THREE.Mesh(geometry,material);
-    mesh.position.set(x,y,z);
-    
-    obj.add(mesh);
-}
-
-function addTableLeg(obj, x,y,z) {
-    'use strict';
-    //var geometry = new THREE.CylinderGeometry(2,2,20,10,10);
-    var geometry = new THREE.CylinderGeometry(2, 2, 21,10,10);
-    var mesh = new THREE.Mesh(geometry,material);
-    mesh.position.set(x,y,z);
-    
-    obj.add(mesh);
-}
-
-function createTable(x,y,z) {
-    'use strict';
-    var table = new THREE.Object3D();
-
-    material = new THREE.MeshBasicMaterial({color:0x00ff00, wireframe: true});
-    
-    //addTableTop(table, 0,17.5,0);
-    addTableTop(table, 0,15, 0);
-    addTableLeg(table, -20,6,-15);
-    addTableLeg(table, -20,6,15);
-    addTableLeg(table, 20,6,15);
-    addTableLeg(table, 20,6,-15);
-    
-    scene.add(table);
-
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
-}
 
 
 
@@ -127,8 +71,9 @@ function addChairWheel(obj, x,y,z) {
 
 function createChair(x,y,z) {
     'use strict';
-    var chair = new THREE.Object3D();
-    
+    chair = new THREE.Object3D();
+    chair.userData = { speed: {x: 0, y: 0, z: 0}, moveKeys: {up:false, down:false, right: false, left: false}, step: 0};
+
     addChairWheel(chair, 4, -9.5, 0);
     addChairWheel(chair, -4, -9.5, 0);
     
@@ -154,14 +99,14 @@ function createChair(x,y,z) {
 
 
 
+
+
 function createCamera() {
     'use strict';
                     // PerspectiveCamera(fov, ratio, near clipping plane, far clipping plane)
-    //camera = new THREE.PerspectiveCamera(70,window.innerWidth / window.innerHeight, 1, 1000);
-    //OrthographicCamera( left : Number, right : Number, top : Number, bottom : Number, near : Number, far : Number )
-    camera = new THREE.OrthographicCamera(-window.innerWidth/10,window.innerWidth/10,window.innerHeight/10,-window.innerHeight/10,0.001,10000);
+    camera = new THREE.PerspectiveCamera(70,window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.x = 50;
-    camera.position.y = 20;
+    camera.position.y = 50;
     camera.position.z = 50;
     camera.lookAt(scene.position);
 }
@@ -170,9 +115,9 @@ function createScene() {
     'use strict';
     scene = new THREE.Scene();
     scene.add(new THREE.AxisHelper(10));
-    createTable(0,0,0);
+
     createChair(0,0,20);
-    createBall(0,0,15);
+    //createchair(0,0,0);
 }
 
 
@@ -197,6 +142,23 @@ function onKeyDown(e) {
     'use strict';
 
     switch(e.keyCode) {
+
+        case 37: //left
+        chair.userData.moveKeys.left = true;
+        
+        break;
+    case 39: //right
+        chair.userData.moveKeys.right = true;
+        break;
+
+    case 38: //up
+        chair.userData.moveKeys.up = true;
+        break;
+    case 40: //down
+        chair.userData.moveKeys.down = true;
+        break;
+
+
     case 65: //A
     case 97: //a
         scene.traverse(function (node) {
@@ -205,22 +167,79 @@ function onKeyDown(e) {
             }
         });
         break;
-    case 83: //S
-    case 115: //s
-        ball.userData.jumping = !ball.userData.jumping;
-        break;
+
+
     }
     //render();
+}
+
+
+
+function onKeyUp(e) {
+    'use strict';
+
+    switch(e.keyCode) {
+    
+    case 37: //left
+        chair.userData.moveKeys.left = false;
+        //chair.userData.speed.x = 0;
+        break;
+    case 39: //right
+        chair.userData.moveKeys.right = false;
+        //chair.userData.speed.x = 0;
+        break;
+    case 38: //up
+        chair.userData.moveKeys.up = false;
+        break;
+    case 40: //down
+        chair.userData.moveKeys.down = false;
+        break;
+    }
 }
 
 function animate() {
     'use strict';
 
-    if (ball.userData.jumping) {
-        ball.userData.step += 0.04;
-        ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
-        ball.position.z = 15 * (Math.cos(ball.userData.step));
+    console.log(chair.userData.speed.x);
+    if (chair.userData.moveKeys.right && chair.userData.speed.x < 0.14) {
+        chair.userData.speed.x += 0.006;
     }
+    if (chair.userData.moveKeys.left && chair.userData.speed.x > -0.14) {
+        chair.userData.speed.x -= 0.006;
+    }
+    if (chair.userData.moveKeys.up && chair.userData.speed.z > -0.6) {
+        chair.userData.speed.z -= 0.06;
+    }
+    if (chair.userData.moveKeys.down && chair.userData.speed.z < 0.6) {
+        chair.userData.speed.z += 0.06;
+    }
+
+    if (chair.userData.speed.x != 0 && (chair.userData.moveKeys.left == false && chair.userData.moveKeys.right == false) ) {
+
+        if (Math.abs(chair.userData.speed.x) > 0.01) {
+            chair.userData.speed.x *= 0.9;
+        }
+        else {
+            chair.userData.speed.x = 0;
+        }
+    }
+    
+    if (chair.userData.speed.z != 0 && (chair.userData.moveKeys.down == false && chair.userData.moveKeys.up == false) ) {
+        if (Math.abs(chair.userData.speed.z) > 0.01) {
+            chair.userData.speed.z *= 0.9;
+        }
+        else {
+            chair.userData.speed.z = 0;
+        }
+    }
+    
+    
+    //chair.position.x += chair.userData.speed.x ;
+    //chair.position.z += chair.userData.speed.z ;
+    chair.rotateY(-chair.userData.speed.x);
+    chair.translateZ(chair.userData.speed.z);
+    
+
     render();
     requestAnimationFrame(animate);
 }
@@ -239,5 +258,6 @@ function init() {
 
     window.addEventListener("resize", onResize);
     window.addEventListener("keydown",onKeyDown);
+    window.addEventListener("keyup",onKeyUp);
 
 }
